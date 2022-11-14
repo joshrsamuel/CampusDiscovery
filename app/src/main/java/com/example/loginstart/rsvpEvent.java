85b92ffee59a8cd1e9f1163059a09decad49a68f
Ishan Kulkarni <ikulkarni8@gatech.edu>
2022-11-14 00:45:32 -0500

    Adding attendees based on RSVP button functionality with database

--- app/src/main/java/com/example/loginstart/rsvpEvent.java
+++ app/src/main/java/com/example/loginstart/rsvpEvent.java
@@ -9,11 +9,15 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class rsvpEvent extends AppCompatActivity{
     TextView title;
@@ -23,6 +27,8 @@ public class rsvpEvent extends AppCompatActivity{
     TextView host;
     DatabaseReference userDatabase;
     Button returnToDashBtn;
+    Button RSVPBtn;
+    FirebaseUser currUser;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,6 +66,31 @@ public class rsvpEvent extends AppCompatActivity{
         location.setText(locationString);
         time.setText(timeString);
         description.setText(descriptionString);
+        final String[] username = new String[1];
+
+        currUser = FirebaseAuth.getInstance().getCurrentUser();
+        userDatabase.child(currUser.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                userInfo user = snapshot.getValue(userInfo.class);
+                username[0] = user.getFullName();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                System.out.println("Failed to get username");
+            }
+        });
+
+        DatabaseReference eventDatabase = FirebaseDatabase.getInstance().getReference("Events");
+        
+        RSVPBtn = (Button) findViewById(R.id.RSVP);
+        RSVPBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                eventDatabase.child(titleString).child("attendees").child(currUser.getUid()).setValue(username[0]);
+            }
+        });
 
         returnToDashBtn = (Button) findViewById(R.id.RSVPreturn);
         returnToDashBtn.setOnClickListener(new View.OnClickListener() {
