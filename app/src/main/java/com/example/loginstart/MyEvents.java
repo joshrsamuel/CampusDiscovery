c250a90f7472cf154ebd31590ac9bdec4a1d8caa
mschlicht3 <mschlicht3@gatech.edu>
2022-11-29 19:56:43 -0500

    Implementing your event functionality.

--- app/src/main/java/com/example/loginstart/MyEvents.java
+++ app/src/main/java/com/example/loginstart/MyEvents.java
@@ -12,6 +12,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -19,12 +21,15 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MyEvents extends AppCompatActivity {
     private Button exitBtn;
     private Button nextBtn;
     private Button backBtn;
     private DatabaseReference mirajDatabase;
+    private DatabaseReference mirajDatabaseEvents;
     protected void onCreate(Bundle savedInstanceState) {
         Context context = this;
         super.onCreate(savedInstanceState);
@@ -36,81 +41,104 @@ public class MyEvents extends AppCompatActivity {
                 startActivity(new Intent(MyEvents.this, student.class));
             }
         });
-        mirajDatabase = FirebaseDatabase.getInstance("https://campusdiscovery-d2e9f-default-rtdb.firebaseio.com/").getReference("Events");
-        final int[] currPage = new int[1];
-        ValueEventListener postListener = new ValueEventListener() {
+        mirajDatabaseEvents = FirebaseDatabase.getInstance("https://campusdiscovery-d2e9f-default-rtdb.firebaseio.com/").getReference("Events");
+        mirajDatabase = FirebaseDatabase.getInstance("https://campusdiscovery-d2e9f-default-rtdb.firebaseio.com/").getReference("UserInfo");
+        FirebaseUser currUser = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference userEvents = mirajDatabase.child(currUser.getUid()).child("events");
+        Set<String> eventData;
+        userEvents.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                ArrayList<Page> pages = new ArrayList<>();
-                ArrayList<DataSnapshot> childData = new ArrayList<>();
-                currPage[0] = 0;
+                Set<String> eventData = new HashSet<>();
                 for (DataSnapshot child : snapshot.getChildren()) {
-                    childData.add(child);
+                    eventData.add(child.getValue(String.class));
+                    System.out.println("data: " + child.getValue(String.class));
                 }
-                int numPages;
-                if (childData.size() % 10 == 0) {
-                    numPages = childData.size() / 10;
-                } else {
-                    numPages = childData.size() / 10 + 1;
-                }
-                int currChild = 0;
-                int pageNum = 1;
-                if (numPages == 0) {
-                    pages.add(new Page(childData, pageNum));
-                }
-                for (int i = 0; i < numPages; i++) {
-                    if (i == numPages - 1) {
-                        pages.add(new Page(childData.subList(currChild, childData.size()), pageNum));
-                        break;
-                    } else {
-                        pages.add(new Page(childData.subList(currChild, currChild + 10), pageNum));
-                        currChild += 10;
-                        pageNum += 1;
-                    }
-                }
-
-                RecyclerView recyclerView = findViewById(R.id.recycleviewEvents);
-                RecyclerViewAdapter adapter = new RecyclerViewAdapter(pages.get(0).getData(), context, MyEvents.this);
-                recyclerView.setAdapter(adapter);
-                recyclerView.setLayoutManager(new LinearLayoutManager(context));
-                Toast.makeText(MyEvents.this, "Events successfully loaded.", Toast.LENGTH_SHORT).show();
-
-                nextBtn = (Button) findViewById(R.id.nextPageEvents);
-                nextBtn.setOnClickListener(new View.OnClickListener() {
+                System.out.println("set: " + eventData);
+                final int[] currPage = new int[1];
+                ValueEventListener postListener = new ValueEventListener() {
                     @Override
-                    public void onClick(View view) {
-                        if (currPage[0] + 1 == pages.size()) {
-                            Toast.makeText(MyEvents.this, "No next page.", Toast.LENGTH_SHORT).show();
-                        } else {
-                            currPage[0] += 1;
-                            RecyclerView recyclerView = findViewById(R.id.recycleviewEvents);
-                            RecyclerViewAdapter adapter = new RecyclerViewAdapter(pages.get(currPage[0]).getData(), context, MyEvents.this);
-                            recyclerView.setAdapter(adapter);
-                            recyclerView.setLayoutManager(new LinearLayoutManager(MyEvents.this));
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        ArrayList<Page> pages = new ArrayList<>();
+                        ArrayList<DataSnapshot> childData = new ArrayList<>();
+                        currPage[0] = 0;
+                        for (DataSnapshot child : snapshot.getChildren()) {
+                            System.out.println(child.child("title").getValue(String.class));
+                            if (eventData.contains(child.child("title").getValue(String.class))) {
+                                childData.add(child);
+                            }
                         }
+                        int numPages;
+                        if (childData.size() % 10 == 0) {
+                            numPages = childData.size() / 10;
+                        } else {
+                            numPages = childData.size() / 10 + 1;
+                        }
+                        int currChild = 0;
+                        int pageNum = 1;
+                        if (numPages == 0) {
+                            pages.add(new Page(childData, pageNum));
+                        }
+                        for (int i = 0; i < numPages; i++) {
+                            if (i == numPages - 1) {
+                                pages.add(new Page(childData.subList(currChild, childData.size()), pageNum));
+                                break;
+                            } else {
+                                pages.add(new Page(childData.subList(currChild, currChild + 10), pageNum));
+                                currChild += 10;
+                                pageNum += 1;
+                            }
+                        }
+
+                        RecyclerView recyclerView = findViewById(R.id.recycleviewEvents);
+                        RecyclerViewAdapter3 adapter = new RecyclerViewAdapter3(pages.get(0).getData(), context);
+                        recyclerView.setAdapter(adapter);
+                        recyclerView.setLayoutManager(new LinearLayoutManager(context));
+                        Toast.makeText(MyEvents.this, "Events successfully loaded.", Toast.LENGTH_SHORT).show();
+
+                        nextBtn = (Button) findViewById(R.id.nextPageEvents);
+                        nextBtn.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                if (currPage[0] + 1 == pages.size()) {
+                                    Toast.makeText(MyEvents.this, "No next page.", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    currPage[0] += 1;
+                                    RecyclerView recyclerView = findViewById(R.id.recycleviewEvents);
+                                    RecyclerViewAdapter3 adapter = new RecyclerViewAdapter3(pages.get(currPage[0]).getData(), context);
+                                    recyclerView.setAdapter(adapter);
+                                    recyclerView.setLayoutManager(new LinearLayoutManager(MyEvents.this));
+                                }
+                            }
+                        });
+                        backBtn = (Button) findViewById(R.id.backPageEvents);
+                        backBtn.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                if (currPage[0] == 0) {
+                                    Toast.makeText(MyEvents.this, "No previous page.", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    currPage[0] -= 1;
+                                    RecyclerView recyclerView = findViewById(R.id.recycleviewEvents);
+                                    RecyclerViewAdapter3 adapter = new RecyclerViewAdapter3(pages.get(currPage[0]).getData(), context);
+                                    recyclerView.setAdapter(adapter);
+                                    recyclerView.setLayoutManager(new LinearLayoutManager(MyEvents.this));
+                                }
+                            }
+                        });
                     }
-                });
-                backBtn = (Button) findViewById(R.id.backPageStud);
-                backBtn.setOnClickListener(new View.OnClickListener() {
                     @Override
-                    public void onClick(View view) {
-                        if (currPage[0] == 0) {
-                            Toast.makeText(MyEvents.this, "No previous page.", Toast.LENGTH_SHORT).show();
-                        } else {
-                            currPage[0] -= 1;
-                            RecyclerView recyclerView = findViewById(R.id.recycleviewEvents);
-                            RecyclerViewAdapter adapter = new RecyclerViewAdapter(pages.get(currPage[0]).getData(), context, student.this);
-                            recyclerView.setAdapter(adapter);
-                            recyclerView.setLayoutManager(new LinearLayoutManager(student.this));
-                        }
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        Toast.makeText(MyEvents.this, "Error. Try Again.", Toast.LENGTH_LONG).show();
                     }
-                });
+                };
+                mirajDatabaseEvents.addValueEventListener(postListener);
             }
+
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
                 Toast.makeText(MyEvents.this, "Error. Try Again.", Toast.LENGTH_LONG).show();
             }
-        };
-        mirajDatabase.addValueEventListener(postListener);
+        });
     }
 }
--- app/src/main/java/com/example/loginstart/RecyclerViewAdapter3.java
+++ app/src/main/java/com/example/loginstart/RecyclerViewAdapter3.java
@@ -26,7 +26,79 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.List;
 
-public class RecyclerViewAdapter3 {
+public class RecyclerViewAdapter3 extends RecyclerView.Adapter<RecyclerViewAdapter3.MyViewHolder> {
 
+    List<DataSnapshot> data;
+    Context context;
+    DatabaseReference userDatabase;
+    DatabaseReference eventDatabase;
+    userInfo eventHost;
+    public RecyclerViewAdapter3(List<DataSnapshot> data, Context context) {
+        this.data = data;
+        this.context = context;
+    }
+    @NonNull
+    @Override
+    public RecyclerViewAdapter3.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.event_cell, parent, false);
+
+        return new RecyclerViewAdapter3.MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.title.setText(data.get(position).child("title").getValue(String.class));
+        holder.date.setText(data.get(position).child("time").getValue(String.class));
+        holder.location.setText(data.get(position).child("location").getValue(String.class));
+        holder.description.setText(data.get(position).child("eventDescription").getValue(String.class));
+        userDatabase = FirebaseDatabase.getInstance().getReference("UserInfo");
+        userDatabase.child(data.get(position).child("host").getValue(String.class)).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                eventHost = snapshot.getValue(userInfo.class);
+                if (eventHost !=null) {
+                    holder.host.setText(eventHost.getFullName());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                System.out.println("Failed");
+            }
+        });
+        holder.editEvent.setVisibility(View.GONE);
+        holder.removeEvent.setVisibility((View.GONE));
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView title, date, location, description, host;
+        FirebaseUser currUser;
+        Button removeEvent;
+        Button editEvent;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            title = itemView.findViewById(R.id.eventTitle);
+            date = itemView.findViewById((R.id.eventDate));
+            location = itemView.findViewById(R.id.eventLocation);
+            description = itemView.findViewById((R.id.eventDes));
+            host = itemView.findViewById(R.id.eventHost);
+
+            currUser = FirebaseAuth.getInstance().getCurrentUser();
+
+            //Edit Button
+            editEvent = itemView.findViewById(R.id.editEvent);
+
+            // Remove Button
+            removeEvent = itemView.findViewById(R.id.removeEvent);
+        }
+    }
 }
 
